be45263da5813fe2c2d04579eee76abca11844ce
xiyang <ls_coder@qq.com>
2018-09-26 15:37:13 +0800

    为pdf组件添加docx-》pdf的能力，对原有PDFService做了包装，新增了入口接口

--- acooly-component-pdf/src/main/java/com/acooly/module/pdf/html/impl/HtmlPdfGenerator.java
+++ acooly-component-pdf/src/main/java/com/acooly/module/pdf/html/impl/HtmlPdfGenerator.java
@@ -4,6 +4,7 @@ import com.acooly.module.pdf.PDFService;
 import com.acooly.module.pdf.PdfGeneratorService;
 import com.acooly.module.pdf.vo.DocumentVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -20,6 +21,7 @@ import java.util.Map;
 @Service("htmlPdfGenerator")
 public class HtmlPdfGenerator implements PdfGeneratorService {
 
+    @Autowired
     private PDFService pdfService;
 
     @Override
