71b3cfc85254b7c4d6012556c8c138132e8c906a
xierui <670173077@qq.com>
2018-10-17 18:57:08 +0800

    Xss

--- acooly-component-chart/src/main/java/com/acooly/module/chart/ChartComponentInitializer.java
+++ acooly-component-chart/src/main/java/com/acooly/module/chart/ChartComponentInitializer.java
@@ -1,17 +1,23 @@
 package com.acooly.module.chart;
 
+
 import org.springframework.context.ConfigurableApplicationContext;
 
 import com.acooly.core.common.boot.component.ComponentInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
+
 /**
  * @author cuifuqiang
  */
 @Slf4j
 public class ChartComponentInitializer implements ComponentInitializer {
+
     @Override
     public void initialize(ConfigurableApplicationContext applicationContext) {
+        setPropertyIfMissing(
+                "acooly.security.xss.exclusions.chart", "/manage/module/chart/chartItems/*.html");
+
     }
 }
