ae556ae45333d57fa874139c88f0df51f6f99466
xiyang <ls_coder@qq.com>
2020-03-27 16:00:32 +0800

    修复ocr识别空指针

--- acooly-component-ocr/src/main/java/com/acooly/module/ocr/service/impl/OcrServiceImpl.java
+++ acooly-component-ocr/src/main/java/com/acooly/module/ocr/service/impl/OcrServiceImpl.java
@@ -56,18 +56,18 @@ public class OcrServiceImpl implements OcrService {
             result.setIdCardNumberType(res.optString("idcard_number_type"));
             if (IdCardSideEnum.front.equals(order.getIdCardSide())) {
                 IdCardOcrFrontDto dto = new IdCardOcrFrontDto();
-                dto.setName(res.optJSONObject("words_result").optJSONObject("姓名").optString("words"));
-                dto.setNation(res.optJSONObject("words_result").optJSONObject("民族").optString("words"));
-                dto.setAddress(res.optJSONObject("words_result").optJSONObject("住址").optString("words"));
-                dto.setIdCardNo(res.optJSONObject("words_result").optJSONObject("公民身份号码").optString("words"));
-                dto.setBirthday(res.optJSONObject("words_result").optJSONObject("出生").optString("words"));
-                dto.setSex(res.optJSONObject("words_result").optJSONObject("性别").optString("words"));
+                dto.setName(res.optJSONObject("words_result").optJSONObject("姓名") != null ? res.optJSONObject("words_result").optJSONObject("姓名").optString("words") : null);
+                dto.setNation(res.optJSONObject("words_result").optJSONObject("民族") != null ? res.optJSONObject("words_result").optJSONObject("民族").optString("words") : null);
+                dto.setAddress(res.optJSONObject("words_result").optJSONObject("住址") != null ? res.optJSONObject("words_result").optJSONObject("住址").optString("words") : null);
+                dto.setIdCardNo(res.optJSONObject("words_result").optJSONObject("公民身份号码") != null ? res.optJSONObject("words_result").optJSONObject("公民身份号码").optString("words") : null);
+                dto.setBirthday(res.optJSONObject("words_result").optJSONObject("出生") != null ? res.optJSONObject("words_result").optJSONObject("出生").optString("words") : null);
+                dto.setSex(res.optJSONObject("words_result").optJSONObject("性别") != null ? res.optJSONObject("words_result").optJSONObject("性别").optString("words") : null);
                 result.setIdCardOcrFrontDto(dto);
             } else {
                 IdCardOcrBackDto dto = new IdCardOcrBackDto();
-                dto.setIssuanceDate(res.optJSONObject("words_result").optJSONObject("签发日期").optString("words"));
-                dto.setExpiryDate(res.optJSONObject("words_result").optJSONObject("失效日期").optString("words"));
-                dto.setAuthority(res.optJSONObject("words_result").optJSONObject("签发机关").optString("words"));
+                dto.setIssuanceDate(res.optJSONObject("words_result").optJSONObject("签发日期") != null ? res.optJSONObject("words_result").optJSONObject("签发日期").optString("words") : null);
+                dto.setExpiryDate(res.optJSONObject("words_result").optJSONObject("失效日期") != null ? res.optJSONObject("words_result").optJSONObject("失效日期").optString("words") : null);
+                dto.setAuthority(res.optJSONObject("words_result").optJSONObject("签发机关") != null ? res.optJSONObject("words_result").optJSONObject("签发机关").optString("words") : null);
                 result.setIdCardOcrBackDto(dto);
             }
         } catch (BusinessException e) {
@@ -92,17 +92,33 @@ public class OcrServiceImpl implements OcrService {
             options.put("detect_direction", String.valueOf(order.isDetectDirection()));
             JSONObject res = client.drivingLicense(order.getImage(), options);
             log.info("驾驶证识别结果:{}", res.toString());
+            JSONObject name = res.optJSONObject("words_result").optJSONObject("姓名");
+            JSONObject idCardNo = res.optJSONObject("words_result").optJSONObject("证号");
+            JSONObject gender = res.optJSONObject("words_result").optJSONObject("性别");
+            JSONObject nationality = res.optJSONObject("words_result").optJSONObject("国籍");
+            JSONObject address = res.optJSONObject("words_result").optJSONObject("住址");
+            JSONObject birthday = res.optJSONObject("words_result").optJSONObject("出生日期");
+            JSONObject firstIssueDate = res.optJSONObject("words_result").optJSONObject("初次领证日期");
+            JSONObject type = res.optJSONObject("words_result").optJSONObject("准驾车型");
+
             result.setLogId(res.optString("log_id"));
-            result.setName(res.optJSONObject("words_result").optJSONObject("姓名").optString("words"));
-            result.setIdCardNo(res.optJSONObject("words_result").optJSONObject("证号").optString("words"));
-            result.setGender(res.optJSONObject("words_result").optJSONObject("性别").optString("words"));
-            result.setNationality(res.optJSONObject("words_result").optJSONObject("国籍").optString("words"));
-            result.setAddress(res.optJSONObject("words_result").optJSONObject("住址").optString("words"));
-            result.setBirthday(res.optJSONObject("words_result").optJSONObject("出生日期").optString("words"));
-            result.setFirstIssueDate(res.optJSONObject("words_result").optJSONObject("初次领证日期").optString("words"));
-            result.setType(res.optJSONObject("words_result").optJSONObject("准驾车型").optString("words"));
-            result.setValidFrom(res.optJSONObject("words_result").optJSONObject("有效期限").optString("words"));
-            result.setValidTo(res.optJSONObject("words_result").optJSONObject("至").optString("words"));
+            result.setName(name != null ? name.optString("words") : null);
+            result.setIdCardNo(idCardNo != null ? idCardNo.optString("words") : null);
+            result.setGender(gender != null ? gender.optString("words") : null);
+            result.setNationality(nationality != null ? nationality.optString("words") : null);
+            result.setAddress(address != null ? address.optString("words") : null);
+            result.setBirthday(birthday != null ? birthday.optString("words") : null);
+            result.setFirstIssueDate(firstIssueDate != null ? firstIssueDate.optString("words") : null);
+            result.setType(type != null ? type.optString("words") : null);
+
+            JSONObject validTo = res.optJSONObject("words_result").optJSONObject("至");
+            if (validTo != null) {
+                result.setValidFrom(res.optJSONObject("words_result").optJSONObject("有效期限") != null ? res.optJSONObject("words_result").optJSONObject("有效期限").optString("words") : null);
+                result.setValidTo(res.optJSONObject("words_result").optJSONObject("至") != null ? res.optJSONObject("words_result").optJSONObject("至").optString("words") : null);
+            } else {
+                result.setValidFrom(res.optJSONObject("words_result").optJSONObject("有效起始日期") != null ? res.optJSONObject("words_result").optJSONObject("有效起始日期").optString("words") : null);
+                result.setValidTo(res.optJSONObject("words_result").optJSONObject("有效期限") != null ? res.optJSONObject("words_result").optJSONObject("有效期限").optString("words") : null);
+            }
         } catch (BusinessException e) {
             log.info("业务异常，原因{}", e.getMessage());
             result.setStatus(ResultStatus.failure);
