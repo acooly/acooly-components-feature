3e03012e4045f7b9737590868d40217125f5df59
xiyang <ls_coder@qq.com>
2020-04-08 11:22:20 +0800

    添加key值注释

--- acooly-component-obs/src/main/java/com/acooly/module/obs/ObsService.java
+++ acooly-component-obs/src/main/java/com/acooly/module/obs/ObsService.java
@@ -18,7 +18,7 @@ public interface ObsService {
      * 传指定文件到指定的bucketName
      *
      * @param bucketName bucketName（为空则使用默认桶配置）
-     * @param key        文件名
+     * @param key        文件名 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      * @param file       上传文件
      */
     OssFile putObject(String bucketName, String key, File file);
@@ -27,7 +27,7 @@ public interface ObsService {
      * 传指定文件到指定的bucketName
      *
      * @param bucketName  bucketName （为空则使用默认桶配置）
-     * @param key         文件名
+     * @param key         文件名 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      * @param inputStream 输入流
      */
     OssFile putObject(String bucketName, String key, InputStream inputStream);
@@ -43,7 +43,7 @@ public interface ObsService {
      * 将指定文件key转换为临时授权的可访问的url
      *
      * @param bucketName   bucketName （为空则使用默认桶配置）
-     * @param key          文件相对地址
+     * @param key          文件相对地址 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      * @param processStyle 图片处理参数，一般用于获取缩略图
      * @param expireDate   过期时间 (为空则使用配置文件中指定的时间，24小时后过期)
      * @return
@@ -54,7 +54,7 @@ public interface ObsService {
      * 传指定文件到指定的bucketName
      *
      * @param bucketName bucketName （为空则使用默认桶配置）
-     * @param key        文件名
+     * @param key        文件名 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      * @param file       文件
      * @param metadata   Object的元数据
      */
@@ -64,7 +64,7 @@ public interface ObsService {
      * 传指定文件到指定的bucketName
      *
      * @param bucketName bucketName （为空则使用默认桶配置）
-     * @param key        文件名
+     * @param key        文件名 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      * @param input      输入流
      * @param metadata   Object的元数据
      */
@@ -74,7 +74,7 @@ public interface ObsService {
      * 下载文件
      *
      * @param bucketName bucketName （为空则使用默认桶配置）
-     * @param key        文件名
+     * @param key        文件名 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      */
     OssFile getObject(String bucketName, String key);
 
@@ -82,7 +82,7 @@ public interface ObsService {
      * 删除文件
      *
      * @param bucketName bucketName （为空则使用默认桶配置）
-     * @param key        文件名
+     * @param key        文件名 长度必须在 1–1023 字节之间,不能以正斜线（/）或者反斜线（\）开头
      * @return 删除结果
      */
     void deleteObject(String bucketName, String key);
