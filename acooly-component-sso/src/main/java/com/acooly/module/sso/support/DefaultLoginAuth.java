8385c5e6be9bacc847d22bd681587d8d4cf51218
lilin <lilin@cartechfin.com>
2021-04-01 01:25:19 +0800

    Revert "sso 安全增强"
    This reverts commit 3a587402

--- acooly-component-sso/src/main/java/com/acooly/module/sso/support/DefaultLoginAuth.java
+++ acooly-component-sso/src/main/java/com/acooly/module/sso/support/DefaultLoginAuth.java
@@ -43,11 +43,6 @@ public class DefaultLoginAuth extends AbstractLoginJwtAuthProcessor<AuthResult> 
             Jwt<Header, Claims> jwt = JWTUtils.parseAuthentication(authentication);
             // 验证 jwt 是否过期
             if (!JWTUtils.validateTimeout(jwt)) {
-
-                //判断jwt是否在黑名单中
-                if(JWTUtils.isBlackToken(jwt)){
-                    return AuthResult.LOGIN_AUTHENTICATION_TIME_OUT;
-                }
                 // 将解析后的信息存入 request 属性中
                 setRequestAttributes(request, jwt);
                 //将subject绑定到线程
