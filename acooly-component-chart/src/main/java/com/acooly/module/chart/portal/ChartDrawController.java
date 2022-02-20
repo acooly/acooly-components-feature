fcd787f85df5d8ee5aa6f421241ca95f611f0d53
zhangpu <zhangpu@prosysoft.com>
2022-02-20 23:46:49 +0800

    增加datarangepicker组件及demo

--- acooly-component-chart/src/main/java/com/acooly/module/chart/portal/ChartDrawController.java
+++ acooly-component-chart/src/main/java/com/acooly/module/chart/portal/ChartDrawController.java
@@ -1,9 +1,9 @@
 /*
-* acooly.cn Inc.
-* Copyright (c) 2018 All Rights Reserved.
-* create by acooly
-* date:2018-10-10
-*/
+ * acooly.cn Inc.
+ * Copyright (c) 2018 All Rights Reserved.
+ * create by acooly
+ * date:2018-10-10
+ */
 package com.acooly.module.chart.portal;
 
 import java.util.List;
@@ -12,6 +12,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.acooly.core.common.web.AbstractJsonEntityController;
+import com.acooly.core.utils.Asserts;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -36,111 +38,135 @@ import com.google.common.collect.Maps;
 
 /**
  * 图表-主题 管理控制器
- * 
+ *
  * @author acooly Date: 2018-10-10 11:15:20
  */
 @Controller
 @RequestMapping(value = "/manage/module/echarts")
-public class ChartDrawController {
+public class ChartDrawController extends AbstractJsonEntityController {
 
-	@Autowired
-	private ChartService chartService;
+    @Autowired
+    private ChartService chartService;
 
-	@Autowired
-	private ChartItemsService chartItemsService;
+    @Autowired
+    private ChartItemsService chartItemsService;
 
-	@Autowired
-	private ChartDataService chartDataService;
+    @Autowired
+    private ChartDataService chartDataService;
 
-	@Autowired
-	private ChartDataQueryService chartDataQueryService;
+    @Autowired
+    private ChartDataQueryService chartDataQueryService;
 
-	@Autowired
-	private ChartDataAnalyseService chartDataAnalyseService;
+    @Autowired
+    private ChartDataAnalyseService chartDataAnalyseService;
 
-	@RequestMapping(value = "index_{id}")
-	public String index(@PathVariable("id") String id, HttpServletRequest request, HttpServletResponse response,
-			Model model) {
-		if (StringUtils.isBlank(id)) {
-			throw new RuntimeException("chartId不能为空,访问格式/index_chartId.html");
-		}
+    @RequestMapping(value = "index_{id}")
+    public String index(@PathVariable("id") String id, HttpServletRequest request, HttpServletResponse response,
+                        Model model) {
+        if (StringUtils.isBlank(id)) {
+            throw new RuntimeException("chartId不能为空,访问格式/index_chartId.html");
+        }
 
-		Chart chart = chartService.get(Long.parseLong(id));
-		if (chart == null) {
-			throw new RuntimeException("chartId不存在的图表");
-		}
+        Chart chart = chartService.get(Long.parseLong(id));
+        if (chart == null) {
+            throw new RuntimeException("chartId不存在的图表");
+        }
 
-		if (chart.getStatus() == StatusEnum.disable) {
-			throw new RuntimeException("图表主题无效");
-		}
+        if (chart.getStatus() == StatusEnum.disable) {
+            throw new RuntimeException("图表主题无效");
+        }
 
-		List<ChartItems> chartItemsList = chartItemsService.findByChartIdAndStatus(chart.getId(), StatusEnum.enable);
-		model.addAttribute("chart", chart);
-		model.addAttribute("chartItemsList", chartItemsList);
+        List<ChartItems> chartItemsList = chartItemsService.findByChartIdAndStatus(chart.getId(), StatusEnum.enable);
+        model.addAttribute("chart", chart);
+        model.addAttribute("chartItemsList", chartItemsList);
 
-		return "/manage/module/echarts/home/index";
-	}
+        return "/manage/module/echarts/home/index";
+    }
 
-	/**
-	 * 解析图形列表
-	 * 
-	 * @param type
-	 * @param chartItemId
-	 * @param request
-	 * @param response
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = "chartItem_{type}_{chartItemId}")
-	public String chartItem(@PathVariable("type") String type, @PathVariable("chartItemId") String chartItemId,
-			HttpServletRequest request, HttpServletResponse response, Model model) {
-		if (StringUtils.isBlank(chartItemId)) {
-			throw new RuntimeException("chartItemId不能为空,访问格式/chartItem_type_chartItemId.html");
-		}
-		ChartItems chartItem = chartItemsService.get(Long.parseLong(chartItemId));
-		if (chartItem == null) {
-			throw new RuntimeException("chartItem不存在的图表");
-		}
-		model.addAttribute("chartItemId", chartItemId);
-		model.addAttribute("loopTime", chartItem.getLoopTime());
-		return "/manage/module/echarts/" + type;
-	}
+    /**
+     * 解析图形列表
+     *
+     * @param type
+     * @param chartItemId
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "chartItem_{type}_{chartItemId}")
+    public String chartItem(@PathVariable("type") String type, @PathVariable("chartItemId") String chartItemId,
+                            HttpServletRequest request, HttpServletResponse response, Model model) {
+        if (StringUtils.isBlank(chartItemId)) {
+            throw new RuntimeException("chartItemId不能为空,访问格式/chartItem_type_chartItemId.html");
+        }
+        ChartItems chartItem = chartItemsService.get(Long.parseLong(chartItemId));
+        if (chartItem == null) {
+            throw new RuntimeException("chartItem不存在的图表");
+        }
+        model.addAttribute("chartItemId", chartItemId);
+        model.addAttribute("loopTime", chartItem.getLoopTime());
+        return "/manage/module/echarts/" + type;
+    }
 
-	/**
-	 * 线形图表,柱形图表,饼图
-	 * 
-	 * @param chartItemId
-	 * @param request
-	 * @param response
-	 * @param model
-	 * @return
-	 */
-	@RequestMapping(value = { "charData_line_{chartItemId}", "charData_bar_{chartItemId}", "charData_pie_{chartItemId}",
-			"charData_bar_stack_{chartItemId}", "charData_map_China_{chartItemId}" })
-	@ResponseBody
-	public JsonResult charData(@PathVariable("chartItemId") String chartItemId, HttpServletRequest request,
-			HttpServletResponse response, Model model) {
-		JsonResult result = new JsonResult();
-		if (StringUtils.isBlank(chartItemId)) {
-			result.setMessage("chartItemId不能为空");
-			result.setSuccess(false);
-			return result;
-		}
+    /**
+     * 线形图表,柱形图表,饼图
+     *
+     * @param chartItemId
+     * @param request
+     * @param response
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = {"charData_line_{chartItemId}", "charData_bar_{chartItemId}", "charData_pie_{chartItemId}",
+            "charData_bar_stack_{chartItemId}", "charData_map_China_{chartItemId}"})
+    @ResponseBody
+    public JsonResult charData(@PathVariable("chartItemId") String chartItemId, HttpServletRequest request,
+                               HttpServletResponse response, Model model) {
+        JsonResult result = new JsonResult();
+        if (StringUtils.isBlank(chartItemId)) {
+            result.setMessage("chartItemId不能为空");
+            result.setSuccess(false);
+            return result;
+        }
 
-		ChartItems chartItems = chartItemsService.get(Long.parseLong(chartItemId));
-		ChartData chartData = chartDataService.findChartDataByItemsId(chartItems.getId());
-		String sql = chartData.getSqlData();
-		List<Map<String, Object>> dbData = chartDataQueryService.querySql(sql);
-		// x轴，y轴 赋值
-		ShaftDataDto shaftDataDto = chartDataAnalyseService.shaftDataAnalyse(chartItems.getTitle(),
-				chartItems.getxShaft(), chartItems.getyShaft(), dbData);
-		result.appendData("shaftData", JSON.toJSON(shaftDataDto));
+        ChartItems chartItems = chartItemsService.get(Long.parseLong(chartItemId));
+        ChartData chartData = chartDataService.findChartDataByItemsId(chartItems.getId());
+        String sql = chartData.getSqlData();
+        List<Map<String, Object>> dbData = chartDataQueryService.querySql(sql);
+        // x轴，y轴 赋值
+        ShaftDataDto shaftDataDto = chartDataAnalyseService.shaftDataAnalyse(chartItems.getTitle(),
+                chartItems.getxShaft(), chartItems.getyShaft(), dbData);
+        result.appendData("shaftData", JSON.toJSON(shaftDataDto));
 
-		// 图表参数设置
-		Map<String, Object> params = Maps.newHashMap();
-		params.put("isShow", chartItems.getIsShow().code());
-		result.appendData("chartItemsParams", JSON.toJSON(params));
-		return result;
-	}
+        // 图表参数设置
+        Map<String, Object> params = Maps.newHashMap();
+        params.put("isShow", chartItems.getIsShow().code());
+        result.appendData("chartItemsParams", JSON.toJSON(params));
+        return result;
+    }
+
+
+    @RequestMapping("list_{chartItemId}")
+    @ResponseBody
+    public JsonResult listMeta(@PathVariable("chartItemId") Long chartItemId, HttpServletRequest request,
+                               HttpServletResponse response, Model model) {
+        JsonResult result = new JsonResult();
+        try {
+            Asserts.notNull(chartItemId, "chartItemId");
+            ChartItems chartItems = chartItemsService.get(chartItemId);
+            ChartData chartData = chartDataService.findChartDataByItemsId(chartItems.getId());
+            String sql = chartData.getSqlData();
+            List<Map<String, Object>> dbData = chartDataQueryService.querySql(sql);
+            String fieldMappedJson = chartData.getFieldMapped();
+            Map<String, String> meta = JSON.parseObject(fieldMappedJson, Map.class);
+            result.appendData("listMeta", meta);
+            result.appendData("listData", dbData);
+            result.appendData("item", chartItems);
+        } catch (Exception e) {
+            handleException(result, "列表", e);
+        }
+        return result;
+    }
+
 
 }
