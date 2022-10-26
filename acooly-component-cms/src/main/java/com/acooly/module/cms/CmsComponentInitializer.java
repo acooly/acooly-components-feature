bd7c2d1860708e9bf06b14934af7775acf82404f
zhangpu <zhangpu@prosysoft.com>
2022-10-26 14:33:29 +0800

    升级为5.2.0后，不存在组件的数据库升级，删除数据库升级配置

--- acooly-component-cms/src/main/java/com/acooly/module/cms/CmsComponentInitializer.java
+++ acooly-component-cms/src/main/java/com/acooly/module/cms/CmsComponentInitializer.java
@@ -26,21 +26,5 @@ public class CmsComponentInitializer implements ComponentInitializer {
         setPropertyIfMissing("acooly.ds.Checker.excludedColumnTables.cms", "cms_content_body");
         setPropertyIfMissing("acooly.security.xss.exclusions.cms[0]", "/manage/module/cms/**");
         setPropertyIfMissing("acooly.security.csrf.exclusions.cms", "/manage/module/cms/**");
-
-        /** 升级cms，增加网页专用标题 */
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_content[0].columnName", "web_title");
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_content[0].patchSql", "ALTER TABLE `cms_content` ADD COLUMN `web_title` VARCHAR(128) NULL COMMENT '网页标题';");
-
-        /**
-         * 升级编码、app封面
-         */
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_content[1].columnName", "appcover");
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_content[1].patchSql", "ALTER TABLE `cms_content` ADD COLUMN `appcover` VARCHAR(255) NULL COMMENT 'app封面';");
-
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_code[0].columnName", "descn");
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_code[0].patchSql", "ALTER TABLE `cms_code` ADD COLUMN `descn` VARCHAR(255) NULL COMMENT '编码描述';");
-
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_code[1].columnName", "type_code");
-        setPropertyIfMissing("acooly.ds.dbPatchs.cms_code[1].patchSql", "ALTER TABLE `cms_code` ADD COLUMN `type_code` VARCHAR(255) NULL COMMENT '类型编码';");
     }
 }
